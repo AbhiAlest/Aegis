35a5f11d57e6cc1372a1acb9fb834a71b83fb8b6
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-20 16:22:34 -0500

    Log on discord bot

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
+import { Client } from 'discord.js';
+import { analyzeSentiment, recognizeEntities, classifyText } from './nlp/nlpProcessing';
+
+const client = new Client();
+
+const token = 'YOUR_BOT_TOKEN';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('message', (message) => {
+  const sentiment = analyzeSentiment(message.content);
+  const entities = recognizeEntities(message.content);
+  const classification = classifyText(message.content);
+
+  console.log('Sentiment:', sentiment);
+  console.log('Entities:', entities);
+  console.log('Classification:', classification);
+});
+
+client.login(token);
 
