ceac73aabf3231e8b4c406a18bf73648ca087c0d
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-20 17:54:38 -0500

    Uses GPT-3.5 for text processing.
    Image and audio processing not completed yet.

--- src/utils/bypassDetection.ts
+++ src/utils/bypassDetection.ts
@@ -1,1 +1,97 @@
+import * as tf from '@tensorflow/tfjs-node';
+import { loadModel, preprocessText, preprocessAudio, preprocessImage, MAX_SEQUENCE_LENGTH } from './model';  // Example functions for model loading and preprocessing
+import { GPT } from '@openai/gpt-3.5'; // import for GPT-3.5 model
+
+let textModel: tf.LayersModel | null = null;
+let audioModel: tf.LayersModel | null = null;
+let imageModel: tf.LayersModel | null = null;
+let gptModel: GPT | null = null; // GPT model
+
+async function loadTextModel(): Promise<tf.LayersModel> {
+  if (textModel === null) {
+    // Load the pre-trained text model (e.g., BERT, GPT)
+    textModel = await loadModel();
+  }
+  return textModel;
+}
+
+async function loadAudioModel(): Promise<tf.LayersModel> {
+  if (audioModel === null) {
+    // Load the pre-trained audio model
+    audioModel = await loadModel();  // Example function to load the audio model
+  }
+  return audioModel;
+}
+
+async function loadImageModel(): Promise<tf.LayersModel> {
+  if (imageModel === null) {
+    // Load the pre-trained image model
+    imageModel = await loadModel();  // Example function to load the image model
+  }
+  return imageModel;
+}
+
+async function loadGPTModel(): Promise<GPT> {
+  if (gptModel === null) {
+    // Load the pre-trained GPT model
+    gptModel = await GPT.load('gpt-3.5-turbo');  // Example function to load the GPT model
+  }
+  return gptModel;
+}
+
+export async function isBypassingFilter(text: string, audio: Float32Array, image: Uint8Array): Promise<boolean> {
+  // Preprocess the text, audio, and image for input to the models
+  const preprocessedText = preprocessText(text);  // Example function for text preprocessing
+  const preprocessedAudio = preprocessAudio(audio);  // Example function for audio preprocessing
+  const preprocessedImage = preprocessImage(image);  // Example function for image preprocessing
+
+  // Load the models
+  const textModel = await loadTextModel();
+  const audioModel = await loadAudioModel();
+  const imageModel = await loadImageModel();
+  const gptModel = await loadGPTModel();
+
+  // Prepare the input tensors for prediction
+  const textInputTensor = tf.tensor2d([preprocessedText], [1, MAX_SEQUENCE_LENGTH]);
+  const audioInputTensor = tf.tensor2d([preprocessedAudio], [1, AUDIO_FEATURE_LENGTH]);
+  const imageInputTensor = tf.tensor2d([preprocessedImage], [1, IMAGE_FEATURE_LENGTH]);
+
+  // Make predictions using the models
+  const textPrediction = textModel.predict(textInputTensor) as tf.Tensor;
+  const audioPrediction = audioModel.predict(audioInputTensor) as tf.Tensor;
+  const imagePrediction = imageModel.predict(imageInputTensor) as tf.Tensor;
+
+  // Combine predictions from different models (e.g., using averaging, ensemble methods)
+  const combinedPrediction = tf.mean([textPrediction, audioPrediction, imagePrediction]);
+
+  // Get the prediction values
+  const predictionValues = combinedPrediction.dataSync();
+
+  // Pass the text through the GPT model for additional analysis
+  const gptPrediction = await gptModel.generate([preprocessedText]); // Example generation with GPT model
+  const gptResult = gptPrediction.choices[0].text;
+
+  // Incorporate additional external resources or attention mechanisms
+
+  // Determine if the message is a bypass attempt based on the prediction
+  const bypassThreshold = 0.7; // Example threshold for bypass classification
+  const safeThreshold = 0.3; // Example threshold for safe classification
+
+  let isBypassAttempt = false;
+  let isSafe = false;
+
+  for (let i = 0; i < predictionValues.length; i++) {
+    if (predictionValues[i] >= bypassThreshold) {
+      isBypassAttempt = true;
+    } else if (predictionValues[i] <= safeThreshold) {
+      isSafe = true;
+    }
+  }
+
+  if (isBypassAttempt && !isSafe) {
+    return true; // The message is classified as a bypass attempt
+  } else {
+    return false; // The message is not a bypass attempt
+  }
+}
 
