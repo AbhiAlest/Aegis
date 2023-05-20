a5d9bd356b939a058bd234ed4d02e36caf13e398
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-20 17:34:53 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits, Message } from 'discord.js';
 import { analyzeSentiment, recognizeEntities, classifyText } from './nlp/nlpProcessing';
+import { detectLanguage, translateMessage } from './translation/translation';
 
-const client = new Client();
+require('dotenv').config();
 
-const token = 'YOUR_BOT_TOKEN';
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
+const client = new Client({ 
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
 });
 
-client.on('message', (message) => {
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.on('messageCreate', async (message: Message) => {
+  if (message.author.bot) return;
+
   const sentiment = analyzeSentiment(message.content);
   const entities = recognizeEntities(message.content);
   const classification = classifyText(message.content);
@@ -17,7 +22,18 @@ client.on('message', (message) => {
   console.log('Sentiment:', sentiment);
   console.log('Entities:', entities);
   console.log('Classification:', classification);
+
+  const detectedLanguage = await detectLanguage(message.content);
+  console.log('Detected Language:', detectedLanguage);
+
+  if (detectedLanguage !== 'en') {
+    const translation = await translateMessage(message.content, 'en');
+    console.log('Translation:', translation);
+    // Handle the translated message
+  } else {
+    console.log('Message is in English:', message.content);
+    // Handle the message directly
+  }
 });
 
-client.login(token);
-
+client.login(process.env.DISCORD_TOKEN);
