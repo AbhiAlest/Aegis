22ade9d8ca7e2758d64dcc6f03ea9adba40a8ab5
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-20 16:27:50 -0500

    Connect with Discord API

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -1,1 +1,17 @@
+import { Client, Message } from 'discord.js';
+
+const client = new Client();
+
+const token = 'YOUR_BOT_TOKEN';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('message', (message: Message) => {
+  // Add bot's logic here
+  // moderation tasks and specific commands
+});
+
+client.login(token);
 
