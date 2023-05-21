1b024d44e9622b0828e0bc3b72657bd57a5b0568
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-21 14:27:03 -0500

    with moderation features

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Client, GatewayIntentBits, Message } from 'discord.js';
 import { analyzeSentiment, recognizeEntities, classifyText } from './nlp/nlpProcessing';
-import { detectLanguage, translateMessage } from './translation/translation';
+import { detectLanguage, translateText } from './translation/translation';
+import { applyModerationRules, loadRules } from './moderation/moderation';
 
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ client.once('ready', () => {
 client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) return;
 
+  // NLP Processing
   const sentiment = analyzeSentiment(message.content);
   const entities = recognizeEntities(message.content);
   const classification = classifyText(message.content);
@@ -23,11 +25,24 @@ client.on('messageCreate', async (message: Message) => {
   console.log('Entities:', entities);
   console.log('Classification:', classification);
 
+  // Moderation
+  const rules = loadRules('rules.txt');
+  const isViolatingRules = applyModerationRules(message.content, rules);
+
+  if (isViolatingRules) {
+    console.log(`Message "${message.content}" violates the rules.`);
+    // Handle the violating message
+  } else {
+    console.log(`Message "${message.content}" is within the rules.`);
+    // Handle the message within the rules
+  }
+
+  // Translation
   const detectedLanguage = await detectLanguage(message.content);
   console.log('Detected Language:', detectedLanguage);
 
   if (detectedLanguage !== 'en') {
-    const translation = await translateMessage(message.content, 'en');
+    const translation = await translateText(message.content, detectedLanguage, 'en');
     console.log('Translation:', translation);
     // Handle the translated message
   } else {
