8ab89a2d2539f2d76c4ed038a69b3b1f36924d40
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-26 15:56:14 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ client.once('ready', () => {
 client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) return;
 
-  // NLP Processing
+  // Natural Language Processing (NLP) 
   const sentiment = analyzeSentiment(message.content);
   const entities = recognizeEntities(message.content);
   const classification = classifyText(message.content);
