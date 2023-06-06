26804ff647b04103dba2e3f39a69a070ffc6acb2
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-06 16:15:28 -0500

    Update imageAnalysis.ts

--- src/image/imageAnalysis.ts
+++ src/image/imageAnalysis.ts
@@ -4,50 +4,75 @@ import * as tfdata from '@tensorflow/tfjs-data';
 
 // Load the pre-trained image analysis models
 async function loadModels() {
-  const model1 = await tf.loadLayersModel('model1/model.json');
-  const model2 = await tf.loadLayersModel('model2/model.json');
-  const model3 = await tf.loadLayersModel('model3/model.json');
-  
-  return [model1, model2, model3];
+  try {
+    const model1 = await tf.loadLayersModel('model1/model.json');
+    const model2 = await tf.loadLayersModel('model2/model.json');
+    const model3 = await tf.loadLayersModel('model3/model.json');
+    
+    return [model1, model2, model3];
+  } catch (error) {
+    console.error('Error loading models:', error);
+    throw error;
+  }
 }
 
 // Preprocess the image data
 function preprocessImage(imageData) {
-  // Convert the image data to a tensor
-  const imageTensor = tf.browser.fromPixels(imageData);
-  
-  // Resize the image to match the input size expected by the models
-  const resizedImage = tf.image.resizeBilinear(imageTensor, [224, 224]);
-  
-  // Normalize the pixel values to be in the range of [0, 1]
-  const normalizedImage = resizedImage.div(255);
-  
-  // Expand the dimensions of the image tensor to match the models' input shape
-  const preprocessedImage = normalizedImage.expandDims(0);
-  
-  return preprocessedImage;
+  try {
+    // Convert the image data to a tensor
+    const imageTensor = tf.browser.fromPixels(imageData);
+    
+    // Resize the image to match the input size expected by the models
+    const resizedImage = tf.image.resizeBilinear(imageTensor, [224, 224]);
+    
+    // Normalize the pixel values to be in the range of [0, 1]
+    const normalizedImage = resizedImage.div(255);
+    
+    // Expand the dimensions of the image tensor to match the models' input shape
+    const preprocessedImage = normalizedImage.expandDims(0);
+    
+    return preprocessedImage;
+  } catch (error) {
+    console.error('Error preprocessing image:', error);
+    throw error;
+  }
 }
 
 // Perform post-processing on the predictions
 function postProcessPredictions(predictions, threshold) {
-  const filteredPredictions = predictions.filter((prediction) => prediction.confidence >= threshold);
-  
-  // Apply additional logic as per your requirements
-  
-  return filteredPredictions;
+  try {
+    const filteredPredictions = predictions
+      .map((prediction) => ({
+        class: prediction.className,
+        confidence: prediction.probability,
+      }))
+      .filter((prediction) => prediction.confidence >= threshold);
+    
+    // Apply additional logic as per your requirements
+    
+    return filteredPredictions;
+  } catch (error) {
+    console.error('Error post-processing predictions:', error);
+    throw error;
+  }
 }
 
 // Apply image augmentation to the input image
 function applyImageAugmentation(image) {
-  const augmentedImage = tfdata.image.randomRotation(
-    tfdata.image.randomFlipLeftRight(
-      tfdata.image.randomFlipUpBottom(
-        tfdata.image.randomCrop(image, [224, 224])
+  try {
+    const augmentedImage = tfdata.image.randomRotation(
+      tfdata.image.randomFlipLeftRight(
+        tfdata.image.randomFlipUpBottom(
+          tfdata.image.randomCrop(image, [224, 224])
+        )
       )
-    )
-  );
-  
-  return augmentedImage;
+    );
+    
+    return augmentedImage;
+  } catch (error) {
+    console.error('Error applying image augmentation:', error);
+    throw error;
+  }
 }
 
 async function analyzeImage(imageData) {
