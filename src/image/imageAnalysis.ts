b98ca79f53883b3d34f09be56bf3e2daee7499d5
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-05 22:15:37 -0500

    Update imageAnalysis.ts

--- src/image/imageAnalysis.ts
+++ src/image/imageAnalysis.ts
@@ -8,7 +8,19 @@ async function loadModel() {
 
 // Preprocess the image data
 function preprocessImage(imageData) {
-  // Preprocessing logic 
+    // Convert the image data to a tensor
+  const imageTensor = tf.browser.fromPixels(imageData);
+
+  // Resize the image to match the input size expected by the model
+  const resizedImage = tf.image.resizeBilinear(imageTensor, [224, 224]);
+
+  // Normalize the pixel values to be in the range of [0, 1]
+  const normalizedImage = resizedImage.div(255);
+
+  // Expand the dimensions of the image tensor to match the model's input shape
+  const preprocessedImage = normalizedImage.expandDims(0);
+
+  return preprocessedImage;
 }
 
 async function analyzeImage(imageData) {
