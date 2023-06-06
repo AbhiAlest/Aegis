159c2974eb0effed613ca08e731b6c1b8fc1e632
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-05 22:48:22 -0500

    Update nlpProcessing.ts

--- src/nlp/nlpProcessing.ts
+++ src/nlp/nlpProcessing.ts
@@ -1,36 +1,57 @@
-import * as natural from 'natural';
+import csvParser from 'csv-parser';
+import fs from 'fs';
 
 const tokenizer = new natural.WordTokenizer();
-const classifier = new natural.BayesClassifier();
 
-function analyzeSentiment(text: string): string {
-  const tokens = tokenizer.tokenize(text);
+async function analyzeSentiment(text: string): Promise<string> {
+  try {
+    const csvFilePath = 'Aegis/src/dataset/train.csv'; // Replace with the actual path to the CSV file
+    const stream = fs.createReadStream(csvFilePath).pipe(csvParser());
 
-  const sentiment = new natural.SentimentAnalyzer('English', natural.PorterStemmer, 'afinn');
-  const sentimentResult = sentiment.getSentiment(tokens);
+    return new Promise<string>((resolve, reject) => {
+      stream.on('data', (row) => {
+        const tweetText = row['5']; // Assuming the text column is at index 5
+        const sentiment = row['0']; // Assuming the polarity column is at index 0
 
-  return sentimentResult;
+        if (tweetText === text) {
+          resolve(sentiment === '4' ? 'positive' : sentiment === '0' ? 'negative' : 'neutral');
+        }
+      });
+
+      stream.on('end', () => {
+        reject(new Error('Sentiment not found in the dataset'));
+      });
+    });
+  } catch (error) {
+    console.error('Error occurred during sentiment analysis:', error);
+    throw error;
+  }
 }
 
 function recognizeEntities(text: string): string[] {
-
   const ner = new natural.NER();
   const entities = ner.getEntities(text);
-
   return entities;
 }
 
 function classifyText(text: string): string {
-
   const classification = classifier.classify(text);
-
   return classification;
 }
 
-// add some example data (FIX LATER)  sentiment analysis
-classifier.addDocument('I love this place', 'positive');
-classifier.addDocument('This is terrible', 'negative');
-classifier.train();
+const classifier = new natural.BayesClassifier();
+
+// Train the classifier with example data from the Kaggle dataset
+fs.createReadStream('Aegis/src/dataset/train.csv')
+  .pipe(csvParser())
+  .on('data', (row) => {
+    const tweetText = row['5']; // Assuming the text column is at index 5
+    const sentiment = row['0']; // Assuming the polarity column is at index 0
+
+    classifier.addDocument(tweetText, sentiment === '4' ? 'positive' : sentiment === '0' ? 'negative' : 'neutral');
+  })
+  .on('end', () => {
+    classifier.train();
+  });
 
 export { analyzeSentiment, recognizeEntities, classifyText };
-
