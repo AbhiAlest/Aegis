0285ce536446b5bb6c5f07beeb41e119edb8e49f
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-13 20:02:52 -0500

    Adds Translation
    ! + prompt results in translation to english.

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -1,6 +1,7 @@
 import { Message } from 'discord.js';
 import { detectLanguage, translateMessage } from '../translation/translation';
 import { isBypassingFilter } from '../utils/bypassDetection';
+import { translateText } from '../translation/translation';
 
 export async function handleMessage(message: Message) {
   // Add your bot's logic here
@@ -24,3 +25,22 @@ export async function handleMessage(message: Message) {
     }
   }
 }
+
+// ...
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('!')) {
+    const command = message.content.slice(1).trim(); // Remove the '!' and trim whitespace
+
+    // Check if the command is for translation
+    if (command.startsWith('translate')) {
+      const textToTranslate = command.slice(9).trim(); // Remove 'translate' and trim whitespace
+
+      // Call the translateText function to translate the text
+      const translatedText = await translateText(textToTranslate);
+
+      // Send the translated text as a reply
+      message.reply(`Translated text: ${translatedText}`);
+    }
+  }
+});
