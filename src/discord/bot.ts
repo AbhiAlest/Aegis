42813d4f6656b17d6bb80578100fe62e12fd66ee
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-06-14 18:59:39 -0500

    Update bot.ts

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -1,7 +1,12 @@
-import { Message } from 'discord.js';
+import { Message, User, GuildMember } from 'discord.js';
 import { detectLanguage, translateMessage } from '../translation/translation';
 import { isBypassingFilter } from '../utils/bypassDetection';
 import { translateText } from '../translation/translation';
+import { analyzeImage } from './imageAnalysis';
+import { analyzeAudio } from './audioAnalysis';
+
+// List of specialized moderators (replace with your own list)
+const specializedModerators = ['moderator1', 'moderator2', 'moderator3'];
 
 export async function handleMessage(message: Message) {
   // Add your bot's logic here
@@ -41,6 +46,112 @@ client.on('messageCreate', async (message) => {
 
       // Send the translated text as a reply
       message.reply(`Translated text: ${translatedText}`);
+    }
+
+    // Check if the command is for image analysis
+    if (command.startsWith('analyze-image') && specializedModerators.includes(message.author.username)) {
+      // Extract the mentioned user from the message
+      const mentionedUser = message.mentions.users.first();
+
+      // Check if a user was mentioned
+      if (!mentionedUser) {
+        message.reply('Please mention a user to perform image analysis.');
+        return;
+      }
+
+      // Get the user's avatar URL
+      const avatarURL = mentionedUser.avatarURL();
+      if (!avatarURL) {
+        message.reply('User does not have an avatar.');
+        return;
+      }
+
+      // Perform image analysis on the user's avatar
+      const analysisResult = await analyzeImage(avatarURL);
+
+      // Take moderation actions based on the analysis result
+      const userToModerate = mentionedUser.username;
+      const days = 7; // Number of days to ban/warn the user
+
+      if (analysisResult.includes('inappropriate')) {
+        // Ban the user
+        const member = message.guild?.member(mentionedUser);
+        if (member) {
+          member.ban({ days, reason: 'Inappropriate content' });
+          message.reply(`User ${userToModerate} has been banned for ${days} days due to inappropriate content.`);
+        }
+      } else if (analysisResult.includes('sensitive')) {
+        // Warn the user
+        message.reply(`User ${userToModerate} has been warned for sensitive content.`);
+      } else {
+        message.reply('No moderation action taken.');
+      }
+    }
+
+    // Check if the command is for audio analysis
+    if (command.startsWith('analyze-audio') && specializedModerators.includes(message.author.username)) {
+      // Extract the mentioned user from the message
+      const mentionedUser = message.mentions.users.first();
+
+      // Check if a user was mentioned
+      if (!mentionedUser) {
+        message.reply('Please mention a user to perform audio analysis.');
+        return;
+      }
+
+      // Get the user's voice channel
+      const voiceChannel = message.guild?.member(mentionedUser)?.voice.channel;
+      if (!voiceChannel) {
+        message.reply('User is not in a voice channel.');
+        return;
+      }
+
+      // Perform audio analysis on the user's voice channel
+      const analysisResult = await analyzeAudio(voiceChannel);
+
+      // Take moderation actions based on the analysis result
+      const userToModerate = mentionedUser.username;
+      const days = 7; // Number of days to ban/warn the user
+
+      if (analysisResult.includes('noise')) {
+        // Ban the user
+        const member = message.guild?.member(mentionedUser);
+        if (member) {
+          member.ban({ days, reason: 'Excessive noise' });
+          message.reply(`User ${userToModerate} has been banned for ${days} days due to excessive noise in voice channel.`);
+        }
+      } else if (analysisResult.includes('inappropriate')) {
+        // Warn the user
+        message.reply(`User ${userToModerate} has been warned for inappropriate audio.`);
+      } else {
+        message.reply('No moderation action taken.');
+      }
+    }
+
+    // Check if the command is for banning a user
+    if (command.startsWith('ban') && specializedModerators.includes(message.author.username)) {
+      // Extract the mentioned user and ban duration from the command
+      const args = command.split(' ').filter(Boolean);
+      if (args.length < 3) {
+        message.reply('Please provide a user and ban duration in days.');
+        return;
+      }
+
+      const mentionedUser = message.mentions.users.first();
+      const banDuration = parseInt(args[2]);
+
+      // Check if a user was mentioned and ban duration is valid
+      if (!mentionedUser || isNaN(banDuration) || banDuration <= 0) {
+        message.reply('Invalid user or ban duration.');
+        return;
+      }
+
+      // Ban the user
+      const member = message.guild?.member(mentionedUser);
+      if (member) {
+        member.ban({ days: banDuration, reason: 'Violation of server rules' });
+        message.reply(`User ${mentionedUser.username} has been banned for ${banDuration} days.`);
+      }
     }
   }
 });
